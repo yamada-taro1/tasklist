e1589654224b2f97a643436ba56e92af38505bdc
taro.yamada <taro.yamada@techacademy.jp>
2021-04-18 02:34:33 +0000

    user signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to:'tasks#index'
     resources :tasks
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :create]
 end
