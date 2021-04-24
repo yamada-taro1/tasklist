cb94bee614e0ed136d5643e42c1557abda6c8669
taro.yamada <taro.yamada@techacademy.jp>
2021-04-24 01:50:41 +0000

    before Delete views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
